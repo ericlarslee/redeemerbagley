503621ff495ac1038208e3f5be326d953a7da661
Eric Lars Lee <ericlarslee@gmail.com>
2021-08-23 08:51:21 -0500

    switched design to use react and react-bootstrap

---
+++ redeemer-webpage/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+      </header> */}
+      <Container fluid>
+        <p>Hello World</p>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
